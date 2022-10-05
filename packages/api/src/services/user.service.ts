540395fdd7a31b798b9b6728e4f3170bb288617b
Shinudesu <arokidaaron93@gmail.com>
2022-10-06 07:33:00 +0800

    chore: add dto validation

--- packages/api/src/services/user.service.ts
+++ packages/api/src/services/user.service.ts
@@ -1,9 +1,13 @@
 import { GetUserDTO } from 'src/dtos/users.dto';
+import { isEmpty } from 'class-validator';
+
 import HTTPException from '../exceptions/HTTPException';
 import Users from '../entities/user.entity';
 
 class UserService {
   getUser = async (userDTO: GetUserDTO): Promise<Users> => {
+    if (isEmpty(userDTO)) throw new HTTPException(401, 'UserDTO is empty!');
+
     const findUser = await Users.findOne({ where: { id: userDTO.id } });
     if (!findUser) throw new HTTPException(404, 'User was not found');
 
