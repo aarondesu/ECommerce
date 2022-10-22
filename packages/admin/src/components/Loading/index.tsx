6b833fbbf081aec1c8840f27b21d79ba75f09787
Shinudesu <arokidaaron93@gmail.com>
2022-10-22 17:56:44 +0800

    style(admin): improved loading screen

--- packages/admin/src/components/Loading/index.tsx
+++ packages/admin/src/components/Loading/index.tsx
@@ -1,9 +1,25 @@
-import { Loader, Center } from '@mantine/core';
+import {
+  Loader, Center, Title, Text, Box,
+} from '@mantine/core';
 
-const Loading = () => (
-  <Center style={{ height: '100vh' }}>
-    <Loader variant="bars" color="dark" />
-  </Center>
-);
+import useStyles from './styles';
+
+const Loading = () => {
+  const { classes } = useStyles();
+
+  return (
+    <Center className={classes.center}>
+      <Box className={classes.box}>
+        <Title order={2} weight={900}>
+          myshopAdmin
+        </Title>
+        <Text color="gray" weight={700} size="sm">
+          Loading...
+        </Text>
+        <Loader variant="bars" color="dark" size="xl" />
+      </Box>
+    </Center>
+  );
+};
 
 export default Loading;
---
+++ packages/admin/src/components/Loading/styles.tsx
@@ -0,0 +1,13 @@
+import { createStyles } from '@mantine/core';
+
+const useStyles = createStyles(() => ({
+  center: {
+    height: '100vh',
+  },
+  box: {
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+}));
+
+export default useStyles;
